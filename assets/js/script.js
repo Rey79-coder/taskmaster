35dfc9bc0852d33a088488ea450c7fd046d6ef9c
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-05-21 19:55:41 -0700

    style.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,28 @@
 var tasks = {};
 
+// 1. CREATE ELEMENT THAT MAKE UP A TASK ITEM.
+
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
-  var taskSpan = $("<span>")
-    .addClass("badge badge-primary badge-pill")
-    .text(taskDate);
-  var taskP = $("<p>")
-    .addClass("m-1")
-    .text(taskText);
+
+  var taskSpan = $("<span>").addClass("badge badge-primary badge-pill").text(taskDate);
+
+  var taskP = $("<p>").addClass("m-1").text(taskText);
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
+
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
 };
+
+
+// 2. SAVE TASK IN LOCAL STORAGE
+
 
 var loadTasks = function() {
   tasks = JSON.parse(localStorage.getItem("tasks"));
@@ -34,15 +41,17 @@ var loadTasks = function() {
   $.each(tasks, function(list, arr) {
     console.log(list, arr);
     // then loop over sub-array
-    arr.forEach(function(task) {
+    $.each(function(task) {
       createTask(task.text, task.date, list);
     });
   });
 };
 
-var saveTasks = function() {
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-};
+
+
+// load tasks for the first time
+loadTasks();
+
 
 // enable draggable/sortable feature on list-group elements
 $(".card .list-group").sortable({
@@ -121,6 +130,7 @@ $("#trash").droppable({
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
   $("#modalTaskDescription, #modalDueDate").val("");
+
 });
 
 // modal is fully visible
@@ -142,7 +152,7 @@ $("#task-form-modal .btn-primary").click(function() {
     $("#task-form-modal").modal("hide");
 
     // save in tasks array
-    tasks.toDo.push({
+    tasks.push({
       text: taskText,
       date: taskDate
     });
@@ -167,7 +177,7 @@ $(".list-group").on("click", "p", function() {
 });
 
 // editable field was un-focused
-$(".list-group").on("blur", "textarea", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current value of textarea
   var text = $(this).val();
 
@@ -187,11 +197,38 @@ $(".list-group").on("blur", "textarea", function() {
   // recreate p element
   var taskP = $("<p>")
     .addClass("m-1")
-    .text(text);
+    .text(text);KeyboardEvent
 
   // replace textarea with new content
   $(this).replaceWith(taskP);
 });
+
+// DATEPICKER INJECTED + A RESTC. FROM PASSED DATES.
+ $("#modalDueDate").datepicker({
+  minDate: 1
+});
+
+
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+};
+
+
 
 // due date was clicked
 $(".list-group").on("click", "span", function() {
@@ -207,8 +244,24 @@ $(".list-group").on("click", "span", function() {
     .val(date);
   $(this).replaceWith(dateInput);
 
+ // DATEPICKER INJECTED + A RESTC. FROM PASSED DATES.
+//  $("#modalDueDate").datepicker({
+//   minDate: 1
+// });
+
+// enable jquery ui datepicker
+dateInput.datepicker({
+  minDate: 1,
+  onClose: function() {
+    // when calendar is closed, force a "change" event on the `dateInput`
+    $(this).trigger("change");
+  }
+});
+
+$('')
   // automatically bring up the calendar
   dateInput.trigger("focus");
+  
 });
 
 // value of due date was changed
@@ -245,5 +298,9 @@ $("#remove-tasks").on("click", function() {
   saveTasks();
 });
 
-// load tasks for the first time
-loadTasks();
+var saveTasks = function() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  
+};
+
+
